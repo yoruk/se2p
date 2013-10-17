50cfe69aff70d5d40eae08e23c23a2d2183d1c79
Michael Schmidt <michael.schmidt1@haw-hamburg.de>
2013-10-18 00:10:44 +0200

    Gate WIP #4

--- src/hal/hw.cpp
+++ src/hal/hw.cpp
@@ -11,25 +11,16 @@ static bool done = false;
 
 void init_HW() {
 	if(!done) {
-#ifdef SIMULATION
-	printf("Simulation is running\n");fflush(stdout);
-	IOaccess_open(); // Open connection to simulation
-#endif
+		// get access rights for hardware
+		if (-1 == ThreadCtl(_NTO_TCTL_IO, 0)) {
+			perror("milestone1_start(): ThreadCtl access failed\n");
+			exit(EXIT_FAILURE);
+		}
 
-	// get access rights for hardware
-	if (-1 == ThreadCtl(_NTO_TCTL_IO, 0)) {
-		perror("milestone1_start(): ThreadCtl access failed\n");
-		exit(EXIT_FAILURE);
-	}
+		// Initialize digital io card
+		out8(DIGITAL_CARD_CONTROL, 0x8A);
 
-	// Initialize digital io card
-	out8(DIGITAL_CARD_CONTROL, 0x8A);
-
-#ifdef SIMULATION
-	IOaccess_close(); // Open connection to simulation
-#endif
-
-	done = true;
+		done = true;
 	}
 }
 
