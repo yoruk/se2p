555ba568e13b3a46a3390c318759efb300709374
Michael Schmidt <michael.schmidt1@haw-hamburg.de>
2013-10-17 18:21:35 +0200

    HW-Init hinzugefuegt

---
+++ src/hal/hw.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <stdbool.h>
+#include "../HWaccess.h"
+#include "hw.h"
+
+static bool done = false;
+
+void init_HW() {
+	if(!done) {
+#ifdef SIMULATION
+	cout << "Simulation is running" << endl;
+	IOaccess_open(); // Open connection to simulation
+#endif
+
+	// get access rights for hardware
+	if (-1 == ThreadCtl(_NTO_TCTL_IO, 0)) {
+		perror("milestone1_start(): ThreadCtl access failed\n");
+		return -1;
+	}
+
+	// Initialize digital io card
+	out8(DIGITAL_CARD_CONTROL, 0x8A);
+
+#ifdef SIMULATION
+	IOaccess_close(); // Open connection to simulation
+#endif
+
+	done = true;
+	}
+}
+
+bool init_HW_Done() {
+	return done;
+}
---
+++ src/hal/hw.h
@@ -0,0 +1,9 @@
+#ifndef HAL_H_
+#define HAL_H_
+
+#include "stdbool.h"
+
+void initHal();
+bool initDone();
+
+#endif /* HAL_H_ */
