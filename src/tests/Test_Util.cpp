258593abc344fe4e6e57df898e468335f59165ad
Michael Schmidt <michael.schmidt1@haw-hamburg.de>
2013-10-19 03:07:19 +0200

    Test_Util.cpp: Ausgabe jetzt in Englisch

--- src/tests/Test_Util.cpp
+++ src/tests/Test_Util.cpp
@@ -10,23 +10,23 @@ void test_Util_start() {
 	printf("Test_Util/test_Util_start()\n\n");
 	printf("reg: 0x%x\n\n", reg);fflush(stdout);
 
-	printf("Bit 1 setzen\n");fflush(stdout);
+	printf("set Bit 1\n");fflush(stdout);
 	setBit(&reg, BIT_0);
-	printf("Ist Bit 1 gesetzt? ");fflush(stdout);
+	printf("Is Bit 1 set? ");fflush(stdout);
 	if(bitIsSet(&reg, BIT_0)) {
-		printf("Ja\n");fflush(stdout);
+		printf("Yes\n");fflush(stdout);
 	} else {
-		printf("Nein\n");fflush(stdout);
+		printf("No\n");fflush(stdout);
 	}
 	printf("reg: 0x%x\n\n", reg);fflush(stdout);
 
-	printf("Bit 1 loeschen\n");fflush(stdout);
+	printf("delete Bit 1\n");fflush(stdout);
 	unsetBit(&reg, BIT_0);
-	printf("Ist Bit 1 gesetzt? ");fflush(stdout);
+	printf("Is Bit 1 set? ");fflush(stdout);
 	if(bitIsSet(&reg, BIT_0)) {
-		printf("Ja\n");fflush(stdout);
+		printf("Yes\n");fflush(stdout);
 	} else {
-		printf("Nein\n");fflush(stdout);
+		printf("No\n");fflush(stdout);
 	}
 	printf("reg: 0x%x\n\n", reg);fflush(stdout);
 }
