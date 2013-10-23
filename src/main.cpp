c7913b45e14e7f11316131a76acc99fa2ca81706
Michael Schmidt <michael.schmidt1@haw-hamburg.de>
2013-10-23 23:47:43 +0200

    aktuelle main()

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,13 @@
 #include "tests/Test_Mutex.h"
 #include "tests/Test_Util.h"
 #include "tests/Test_Gate.h"
+#include "tests/Test_Simple_Gate.h"
 #include "tests/Test_TrafficLight.h"
 #include "tests/Test_Conveyor.h"
 #include "tests/Test_Led.h"
+#include "tests/Test_Serial.h"
 #include "HWaccess.h"
 #include "milestone2/Simple_Milestone2.h"
-
-// zum debuggen
-#include "unistd.h"
-#include "Global.h"
-#include "hal/Util.h"
-#include "hal/hw.h"
-#include "hal/Gate.h"
-
 
 int main(int argc, char *argv[]) {
 #ifdef SIMULATION
@@ -28,27 +22,15 @@ int main(int argc, char *argv[]) {
 	//return test_Mutex_start();
 	//test_Util_start();
 
-	//test_Gate_start();
+	test_Gate_start();
 	//test_Led_start();
 	//test_TrafficLight_start();
 	//test_Conveyor_start();
+	//test_Simple_Gate_start();
+	//test_Serial_start();
 
-	simple_milestone2_start();
+	//simple_milestone2_start();
 
-	// DEBUG!!!
-
-//	printf("real-life test!\n");fflush(stdout);
-//	Gate* gate = Gate::getInstance();
-//
-//	init_HW();
-//
-//	gate->open();
-//	printf("bit set\n");fflush(stdout);
-//
-//	sleep(2);
-//
-//	gate->close();
-//	printf("bit unset\n");fflush(stdout);
 
 #ifdef SIMULATION
 	IOaccess_close(); // Open connection to simulation
