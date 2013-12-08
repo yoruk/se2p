7004c880e5b07f563ca0d9003000b26d2ff91f5f
Nebal el Bebbili <nebal.elbebbili@haw-hamburg.de>
2013-12-09 00:45:53 +0100

    Test erweitert

--- src/tests/Test_TrafficLight.cpp
+++ src/tests/Test_TrafficLight.cpp
@@ -7,20 +7,34 @@ void test_TrafficLight_start(){
 	TrafficLight* light = TrafficLight::getInstance();
 	int i;
 
-	for(i=0; i<RUNTIME; i++) {
-		light->greenOn();
-		sleep(1);
-		light->greenOff();
-		sleep(1);
-		light->redOn();
-		sleep(1);
-		light->redOff();
-		sleep(1);
-		light->yellowOn();
-		sleep(1);
-		light->yellowOff();
-		sleep(1);
-	}
+//		for (i = 0; i < RUNTIME; i++) {
+//		light->greenOn();
+//		sleep(1);
+//		light->greenOff();
+//		sleep(1);
+//		light->redOn();
+//		sleep(1);
+//		light->redOff();
+//		sleep(1);
+//		light->yellowOn();
+//		sleep(1);
+//		light->yellowOff();
+//		sleep(1);
+//	}
+	light->greenOn();
+	sleep(2);
+	light->greenOff();
+	light->greenOff();
+	sleep(2);
+	light->greenOn();
+	sleep(2);
+	light->greenOff();
+	light->greenOff();
+	light->yellowOn();
+//	light->flashRedOn();
+//	sleep(6);
+//	light->flashRedOff();
+//	sleep(2);
 
 }
 
