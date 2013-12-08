e5e5f1a8719651635e87d732dc0fd34b6c72195d
Nebal el Bebbili <nebal.elbebbili@haw-hamburg.de>
2013-12-09 00:35:44 +0100

    weiteres beispiel

---
+++ src/examples/msgSendReceivePulse_2x.cpp
@@ -0,0 +1,122 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <sys/neutrino.h>
+#include <errno.h>
+
+#define RUNTIME 20
+#define WAIT 5
+#define WAIT_SHORT 5
+
+static pthread_t t1, t2, t3;
+static bool run = true;
+static int channelId = -1;
+
+static void* server(void* arg) {
+	int number = 0;
+	struct _pulse pulse;
+	int res = 0;
+
+	channelId = ChannelCreate(0);
+	if(channelId == -1) {
+		printf("msgSendReceivePulse: server: error creating channel!\n");fflush(stdout);
+		exit(EXIT_FAILURE);
+	}
+
+	sleep(5);
+
+	while(run) {
+		res = MsgReceivePulse(channelId, &pulse, sizeof(pulse), NULL);
+		if(res  == -1) {
+			printf("msgSendReceivePulse: server: error receiving pulse!\n");fflush(stdout);
+			exit(EXIT_FAILURE);
+		}
+
+		number++;
+		printf("Server: Pulse message #%d received\n", number);fflush(stdout);
+	}
+
+	res = ChannelDestroy(channelId);
+	if(res == -1) {
+		printf("msgSendReceivePulse: server: error destroying channel!\n");fflush(stdout);
+		exit(EXIT_FAILURE);
+	}
+
+	pthread_exit(NULL);
+}
+
+static void* server2(void* arg) {
+	int number = 0;
+	struct _pulse pulse;
+	int res = 0;
+
+	while(run) {
+		res = MsgReceivePulse(channelId, &pulse, sizeof(pulse), NULL);
+		if(res  == -1) {
+			printf("msgSendReceivePulse: server: error receiving pulse!\n");fflush(stdout);
+			exit(EXIT_FAILURE);
+		}
+
+		number++;
+		printf("Server2: Pulse message #%d received\n", number);fflush(stdout);
+	}
+
+	pthread_exit(NULL);
+}
+
+
+static void* client(void* arg) {
+	int nodeId = 0; // 0 = local
+	pid_t pid = 0; // 0 = same process
+	int connectionId = 0;
+	int number = 0;
+	int res = 0;
+
+	connectionId = ConnectAttach(nodeId, pid, channelId, 0, 0);
+	if(connectionId == -1) {
+		printf("msgSendReceivePulse: client: error attaching to channel!\n");fflush(stdout);
+		exit(EXIT_FAILURE);
+	}
+
+	while(run) {
+		sleep(WAIT_SHORT);
+
+		number++;
+		printf("Client: sending pulse message #%d\n", number);fflush(stdout);
+
+		res = MsgSendPulse(connectionId, SIGEV_PULSE_PRIO_INHERIT, 1, 0);
+		if(res == -1) {
+			printf("msgSendReceivePulse: client: error sending pulse!\n");fflush(stdout);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	ConnectDetach(connectionId);
+	if(res == -1) {
+		printf("msgSendReceiveReply: client: error detaching from channel!\n");fflush(stdout);
+		exit(EXIT_FAILURE);
+	}
+
+	pthread_exit(NULL);
+}
+
+
+
+
+void msgSendReceivePulse_2x_start() {
+	printf("running msgSendReceivePulse_start()\n");fflush(stdout);
+
+	pthread_create(&t1, NULL, &server, NULL);
+	pthread_create(&t2, NULL, &server2, NULL);
+	pthread_create(&t3, NULL, &client, NULL);
+
+	sleep(RUNTIME);
+
+	run = false;
+
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
+}
---
+++ src/examples/msgSendReceivePulse_2x.h
@@ -0,0 +1,6 @@
+#ifndef MSGSENDRECEIVEPULSE_2X_H_
+#define MSGSENDRECEIVEPULSE_2X_H_
+
+void msgSendReceivePulse_2x_start();
+
+#endif /* MSGSENDRECEIVEPULSE_2X_H_ */
