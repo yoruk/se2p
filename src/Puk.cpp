6b0d43660cd35eb31068ec24d6db916e471e6eb4
Besnik Mulici <Bruce05@hotmail.de>
2013-12-12 19:04:49 +0100

    Puk fertig implementiert

--- src/Puk.cpp
+++ src/Puk.cpp
@@ -1,12 +1,13 @@
 #include "Puk.h"
 
-int Puk::count = 0;
+int Puk::count = -BUFFER_LENGHT;
 
 Puk::Puk() {
 	count++;
 	id = count;
 	typ = 0;
-	hoehenmessung = 0;
+	hoehenmessung1 = 0;
+	hoehenmessung2 = 0;
 	status_flach = false;
 }
 
@@ -20,9 +21,14 @@ int Puk::get_id(){
 char Puk::get_typ(){
 	return typ;
 }
-int Puk::get_hoehenmessung (){
-	return hoehenmessung;
+int Puk::get_hoehenmessung1 (){
+	return hoehenmessung1;
 }
+
+int Puk::get_hoehenmessung2 (){
+	return hoehenmessung2;
+}
+
 bool Puk::get_status_flach(){
 	return status_flach;
 }
@@ -31,9 +37,14 @@ bool Puk::get_status_flach(){
 void Puk::set_typ(char par){
 	typ = par;
 }
-void Puk::set_hoehenmessung(int par){
-	hoehenmessung = par;
+void Puk::set_hoehenmessung1(int par){
+	hoehenmessung1 = par;
 }
+
+void Puk::set_hoehenmessung2(int par){
+	hoehenmessung2 = par;
+}
+
 void Puk::set_status_flach(bool par){
 	status_flach = par;
 }
--- src/Puk.h
+++ src/Puk.h
@@ -1,6 +1,8 @@
 #ifndef PUK_H_
 #define PUK_H_
 
+#include "Global.h"
+
 class Puk {
 public:
 	Puk();
@@ -8,19 +10,22 @@ public:
 	//Getter
 	int get_id();
 	char get_typ();
-	int get_hoehenmessung ();
+	int get_hoehenmessung1 ();
+	int get_hoehenmessung2 ();
 	bool get_status_flach();
 
 	//Setter
 	void set_typ(char par);
-	void set_hoehenmessung(int par);
+	void set_hoehenmessung1(int par);
+	void set_hoehenmessung2(int par);
 	void set_status_flach(bool par);
 
 private:
 	static int count;
 	int id;
 	char typ;
-	int hoehenmessung;
+	int hoehenmessung1;
+	int hoehenmessung2;
 	bool status_flach;
 };
 
