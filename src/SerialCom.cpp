77096294c8d456456b47ec1dab5d5a40f7b446b5
Michael Schmidt <michael.schmidt1@haw-hamburg.de>
2013-12-15 02:00:13 +0100

    SerialCom: umgebaut auf thread-object, jetzt komplett funktional

--- src/SerialCom.cpp
+++ src/SerialCom.cpp
@@ -1,112 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
-#include <pthread.h>
 #include <sys/neutrino.h>
 #include "hal/Serial.h"
 #include "Mutex.h"
 #include "Global.h"
 #include "SerialCom.h"
 
+typedef struct __attribute__((__packed__)) {
+		uint8_t is_msg;
+		uint8_t msg_content;
+		uint16_t puk_id;
+		uint8_t puk_type;
+		uint16_t puk_height;
+} package;
+
 /// This class's methods can transfer packages
 /// with puk information or status messages over the serial line.
 /// It also holds a background process
 /// which receives packages and informs the dispatcher about them.
 
-typedef struct __attribute__((__packed__)) {
-		uint8_t is_msg;
-		uint8_t msg_content;
-		uint8_t puk_id;
-		uint8_t puk_type;
-		uint16_t puk_height;
-} package;
-
 static Mutex* mutex = new Mutex(); /// the mutex for controlling the access
-static SerialCom* serialCom; /// the SerialCom object itself
 static Serial* serial; /// the Serial object for basic i/o-functions
-static pthread_t rcv_pkgs;
-
-/// this is a pthread who receives packages,
-///	checks their content
-/// and sends suitable pulse messages to the dispatcher
-void* receive_pkgs(void* arg) {
-	int nodeId = 0; // 0 = local
-	pid_t pid = 0; // 0 = same process
-	int connectionId = 0;
-	int res = 0;;
-	package p;
-	int pulse_val = 0;
-
-	connectionId = ConnectAttach(nodeId, pid, *(int*)arg, 0, 0);
-	if(connectionId == -1) {
-		perror("SerialCom: ERROR, couldn't attach to channel");fflush(stdout);
-		exit(EXIT_FAILURE);
-	}
-
-	while(TRUE) {
-		res = serial->read_serial((unsigned char*)&p, sizeof(p));
-
-		// was the transfer successfully ?
-		if(res == sizeof(p)) {
-
-			// what type of package ?
-			if(p.is_msg) {
-
-				// package holds a message
-				res = MsgSendPulse(connectionId, SIGEV_PULSE_PRIO_INHERIT, PULSE_MSG_FROM_CONVEYOR, p.msg_content);
-				if(res == -1) {
-					perror("SerialCom: ERROR, couldn't send pulse message with puk information");fflush(stdout);
-					exit(EXIT_FAILURE);
-				}
-
-				//DEBUG
-//				printf("pulse message with code = %d, value = %d has been sent\n",
-//						PULSE_MSG_FROM_CONVEYOR, p.msg_content);fflush(stdout);
-
-			} else {
-
-				// package holds puk information
-				pulse_val = p.puk_id;
-				pulse_val <<= 8;
-				pulse_val |= p.puk_type;
-				pulse_val <<= 16;
-				pulse_val |= p.puk_height;
-
-				res = MsgSendPulse(connectionId, SIGEV_PULSE_PRIO_INHERIT, PULSE_PUK_INFORMATION, pulse_val);
-				if(res == -1) {
-					perror("SerialCom: ERROR, couldn't send pulse message with puk information");fflush(stdout);
-					exit(EXIT_FAILURE);
-				}
-
-				//DEBUG
-//				printf("pulse message with code = %d, value = %d has been sent\n",
-//						PULSE_PUK_INFORMATION, pulse_val);fflush(stdout);
-			}
-
-
-		} else {
-			perror("SerialCom: ERROR, package receive failed");fflush(stdout);
-		}
-
-	}
-
-	pthread_exit(NULL);
-}
+static SerialCom* serialCom; /// the SerialCom object itself
+static int channel_id; /// channel to the dispatcher
 
 /// Serialcom-constructor
 SerialCom::SerialCom() {
 	serial = Serial::getInstance();
+	serial->open_serial();
 }
 
 /// SerialCom-deconstructor
 SerialCom::~SerialCom() {
-	if(pthread_cancel(rcv_pkgs) != 0) {
-		perror("SerialCom: ERROR, couldn't cancel receive_pkgs() properly");fflush(stdout);
-	}
-
-	pthread_join(rcv_pkgs, NULL);
+	serial->close_serial();
 
 	delete serial;
+	serial = NULL;
+
 	delete serialCom;
 	serialCom = NULL;
 }
@@ -122,28 +53,6 @@ SerialCom* SerialCom::getInstance() {
 	mutex->unlock();
 
 	return serialCom;
-}
-
-/// starts the background process for receiving packages
-///
-/// \param Chid channel id where pulse messages should go to
-///
-/// \return 0=successful, 1=failed
-int SerialCom::start_pkg_receiver(int Chid) {
-	int res = 0;
-
-	//printf("DEBUG:SerialCom: starting package receiver\n");fflush(stdout);
-
-	res = pthread_create(&rcv_pkgs, NULL, &receive_pkgs, (void*)Chid);
-	if(res != 0) {
-		perror("SerialCom: ERROR, couldn't start receive_pkgs() properly");fflush(stdout);
-
-		return res;
-	}
-
-	pthread_detach(rcv_pkgs);
-
-	return res;
 }
 
 /// send a package with puk data
@@ -167,7 +76,7 @@ int SerialCom::send_puk_data_pkg(int puk_id, int puk_type, int puk_height) {
 	p.puk_height = (uint16_t)puk_height;
 
 	// sending package
-	res = serial->read_serial((unsigned char*)&p, sizeof(p));
+	res = serial->write_serial((unsigned char*)&p, sizeof(p));
 
 	return (res == sizeof(p)) ? 0 : 1;
 }
@@ -185,7 +94,7 @@ int SerialCom::send_msg_pkg(int msg_content) {
 
 	// wrapping package
 	p.is_msg = TRUE;
-	p.msg_content = msg_content;
+	p.msg_content = (uint8_t)msg_content;
 	p.puk_id = 0;
 	p.puk_type = 0;
 	p.puk_height = 0;
@@ -195,3 +104,88 @@ int SerialCom::send_msg_pkg(int msg_content) {
 
 	return (res == sizeof(p)) ? 0 : 1;
 }
+
+/// sets the channel id, needs to be set BEFORE the background thread is started
+///
+/// \param chid channel id for communication with dispatcher
+///
+/// \return 0=successful, 1=failed
+int SerialCom::set_Channel(int chid) {
+	if(!serialCom) {
+		return 1;
+	} else {
+		channel_id = chid;
+
+		return 0;
+	}
+}
+
+/// this is thread receives packages,
+///	checks their content
+/// and sends suitable pulse messages to the dispatcher
+void SerialCom::execute(void *arg) {
+	int nodeId = 0; // 0 = local
+	pid_t pid = 0; // 0 = same process
+	int connectionId = 0;
+	int res = 0;;
+	package p;
+	int pulse_val = 0;
+
+	connectionId = ConnectAttach(nodeId, pid, channel_id, 0, 0);
+	if(connectionId == -1) {
+		perror("SerialCom: ERROR, couldn't attach to channel");fflush(stdout);
+		exit(EXIT_FAILURE);
+	}
+
+	while(!isStopped() && (channel_id != 0)) {
+		// read package
+		res = serial->read_serial((unsigned char*)&p, sizeof(p));
+
+		// was the transfer successfully ?
+		if(res == sizeof(p)) {
+
+			// what type of package ?
+			if(p.is_msg) {
+
+				// package holds a message
+				res = MsgSendPulse(connectionId, SIGEV_PULSE_PRIO_INHERIT, PULSE_MSG_FROM_CONVEYOR, p.msg_content);
+				if(res == -1) {
+					perror("SerialCom: ERROR, couldn't send pulse message with puk information");fflush(stdout);
+					exit(EXIT_FAILURE);
+				}
+
+				//DEBUG
+//				printf("pulse message with code = %d, value = %d has been sent\n",
+//						PULSE_MSG_FROM_CONVEYOR, p.msg_content);fflush(stdout);
+
+			} else {
+
+				// package holds puk information
+				pulse_val = p.puk_id;
+				pulse_val <<= PUK_TYPE_LENGTH;
+				pulse_val |= p.puk_type;
+				pulse_val <<= PUK_HEIGHT_LENGTH;
+				pulse_val |= p.puk_height;
+
+				res = MsgSendPulse(connectionId, SIGEV_PULSE_PRIO_INHERIT, PULSE_PUK_INFORMATION, pulse_val);
+				if(res == -1) {
+					perror("SerialCom: ERROR, couldn't send pulse message with puk information");fflush(stdout);
+					exit(EXIT_FAILURE);
+				}
+
+				//DEBUG
+//				printf("pulse message with code = %d, value = %d has been sent\n",
+//						PULSE_PUK_INFORMATION, pulse_val);fflush(stdout);
+			}
+
+
+		} else {
+			perror("SerialCom: ERROR, package receive failed");fflush(stdout);
+		}
+	}
+}
+
+void SerialCom::shutdown() {
+
+}
+
--- src/SerialCom.h
+++ src/SerialCom.h
@@ -1,16 +1,22 @@
 #ifndef SERIALCOM_H_
 #define SERIALCOM_H_
 
-class SerialCom {
+#include <HAWThread.h>
+
+class SerialCom: public thread::HAWThread {
 	public:
 		~SerialCom();
 		static SerialCom* getInstance();
-		int start_pkg_receiver(int Chid);
 		int send_puk_data_pkg(int puk_id, int puk_type, int puk_height);
 		int send_msg_pkg(int msg_content);
+		int set_Channel(int chid);
 
 	private:
 		SerialCom();
+
+	protected:
+		virtual void execute(void* arg);
+		virtual void shutdown();
 };
 
 #endif /* SERIALCOM_H_ */
